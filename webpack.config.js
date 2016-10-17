81216587f31e9e53ebca94ad4165b4fd49adf509
kikunantoka <kikunantoka@gmail.com>
2016-10-17 17:30:07 +0900

    Add minimum webpack settings

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    bundle: path.join(__dirname, '/front/javascripts/app.js')
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, '/public/assets/javascripts/'),
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js[x]?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.svg$/, loader: 'url-loader?mimetype=image/svg+xml' },
+      { test: /\.woff$/, loader: 'url-loader?mimetype=application/font-woff' },
+      { test: /\.woff2$/, loader: 'url-loader?mimetype=application/font-woff' },
+      { test: /\.eot$/, loader: 'url-loader?mimetype=application/font-woff' },
+      { test: /\.ttf$/, loader: 'url-loader?mimetype=application/font-woff' },
+    ],
+  }
+}
