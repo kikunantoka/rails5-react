328faf8540ba772c182850b79a53a3526d4f7a45
kikunantoka <kikunantoka@gmail.com>
2016-10-21 16:04:18 +0900

    Add css file loader for production

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,7 @@
 module ApplicationHelper
+  def css_load_tag(file_name)
+    stylesheet_link_tag assets_path("#{file_name}.css") if Rails.env.production?
+  end
   def assets_path(path)
     return "http://localhost:8080/#{path}" if Rails.env.development?
     manifest = Rails.application.config.assets_manifest
