b095eb65cbefa0a4f241c569385ff1ee8eaae172
kikunantoka <kikunantoka@gmail.com>
2016-11-01 22:34:35 +0900

    Add postcss-import

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,10 +1,14 @@
-const path = require('path');
-const webpack = require('webpack');
-const cssnext = require('postcss-cssnext');
+const path = require('path')
+const webpack = require('webpack')
+const cssimport = require('postcss-import')
+const cssnext = require('postcss-cssnext')
 
 module.exports = {
   entry: {
-    bundle: path.join(__dirname, '/front/src/js/app.js')
+    bundle: path.join(__dirname, '/front/src/js/app.js'),
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
   },
   output: {
     filename: '[name].js',
@@ -23,7 +27,7 @@ module.exports = {
         loaders: [
           'style',
           'css?modules&localIdentName=[name]--[local]--[hash:base64:5]',
-          'postcss'
+          'postcss',
         ],
         exclude: /node_modules/,
       },
@@ -34,16 +38,16 @@ module.exports = {
       },
       {
         test: /\.(jpeg|jpg|png|gif)$/,
-        loader: "file?name=img/[name].[ext]"
+        loader: 'file?name=img/[name].[ext]',
       },
       {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url?limit=10000&mimetype=application/font-woff'
+        loader: 'url?limit=10000&mimetype=application/font-woff',
       },
       {
         test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'file'
-      }
+        loader: 'file',
+      },
     ],
   },
   plugins: [
@@ -53,8 +57,8 @@ module.exports = {
       },
     }),
   ],
-  postcss: function() {
-    return [cssnext];
+  postcss() {
+    return [cssimport, cssnext]
   },
   devServer: {
     contentBase: '/public/assets/',
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,12 +1,16 @@
-const path = require('path');
-const webpack = require('webpack');
-const ManifestPlugin = require('webpack-manifest-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const cssnext = require('postcss-cssnext');
+const path = require('path')
+const webpack = require('webpack')
+const ManifestPlugin = require('webpack-manifest-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const cssimport = require('postcss-import')
+const cssnext = require('postcss-cssnext')
 
 module.exports = {
   entry: {
-    bundle: path.join(__dirname, '/front/src/js/app.js')
+    bundle: path.join(__dirname, '/front/src/js/app.js'),
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
   },
   output: {
     filename: '[name]-[hash].js',
@@ -26,25 +30,25 @@ module.exports = {
           'style',
           ['css?modules&localIdentName=[name]--[local]--[hash:base64:5]', 'postcss']
         ),
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.css$/,
-        loader: ExtractTextPlugin.extract('style','css'),
+        loader: ExtractTextPlugin.extract('style', 'css'),
         include: /node_modules/,
       },
       {
         test: /\.(jpeg|jpg|png|gif)$/,
-        loader: "file?name=img/[name]-[hash].[ext]"
+        loader: 'file?name=img/[name]-[hash].[ext]',
       },
       {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url?limit=10000&mimetype=application/font-woff&name=[name].[ext]'
+        loader: 'url?limit=10000&mimetype=application/font-woff&name=[name].[ext]',
       },
       {
         test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'file?name=[name].[ext]'
-      }
+        loader: 'file?name=[name].[ext]',
+      },
     ],
   },
   plugins: [
@@ -62,7 +66,7 @@ module.exports = {
     new ManifestPlugin({ fileName: 'webpack-manifest.json' }),
     new ExtractTextPlugin('[name]-[hash].css', { allChunks: true }),
   ],
-  postcss: function() {
-    return [cssnext];
-  }
+  postcss() {
+    return [cssimport, cssnext]
+  },
 }
