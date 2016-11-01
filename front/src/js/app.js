a2c808dfe878d3d0206e4b4a7cfd9b984f5866ab
kikunantoka <kikunantoka@gmail.com>
2016-11-01 22:42:25 +0900

    Fix according to eslint

--- front/src/js/app.js
+++ front/src/js/app.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Button} from 'react-bootstrap';
-import {Icon} from 'react-fa';
-import styles from '../css/main.css';
-import image from '../img/animation-01.gif';
-
-var CommentBox = React.createClass({
-  render: function() {
-    return (
-      <div>
-        <p>Hello, world! I am a <span className={styles.importantText}>CommentBox</span>.<Icon spin name="spinner" /></p>
-        <Button bsStyle="primary">Primary</Button>
-        <h2>This is background-image.</h2>
-        <div className={styles.image}></div>
-        <h2>This is img tag.</h2>
-        <img src={image}/>
-      </div>
-    );
-  }
-});
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Button } from 'react-bootstrap'
+import { Icon } from 'react-fa'
+import styles from '../css/main.css'
+import image from '../img/animation-01.gif'
 
 ReactDOM.render(
-  <CommentBox />,
+  <div>
+    <p>Hello, world! I am a <span className={styles.importantText}>CommentBox</span>.<Icon spin name="spinner" /></p>
+    <Button bsStyle="primary">Primary</Button>
+    <h2>This is background-image.</h2>
+    <div className={styles.image} />
+    <h2>This is img tag.</h2>
+    <img src={image} alt="hoge" />
+  </div>,
   document.getElementById('app')
-);
+)
